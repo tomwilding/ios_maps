74e55d89d3b94da3ced9fc82c88189905bc50c0c
thomas.wilding <thomas.wilding@accenture.com>
2017-04-01 13:26:17 +0100

    location search enabled

--- gmaps_demo/ViewController.swift
+++ gmaps_demo/ViewController.swift
@@ -1,6 +1,10 @@
 import UIKit
 import MapKit
 
+protocol HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark)
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
@@ -8,6 +12,8 @@ class ViewController: UIViewController {
     var currentLocation: CLLocation?
     
     var resultSearchController:UISearchController? = nil
+    
+    var selectedPin:MKPlacemark? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +26,25 @@ class ViewController: UIViewController {
             locationManager.distanceFilter = 50
             locationManager.startUpdatingLocation()
             locationManager.delegate = self
+            
+            
+            // Setup Search Bar
+            let locationSearchTable = storyboard!.instantiateViewController(withIdentifier: "LocationSearchTable") as! LocationSearchTable
+            resultSearchController = UISearchController(searchResultsController: locationSearchTable)
+            resultSearchController?.searchResultsUpdater = locationSearchTable
+            let searchBar = resultSearchController!.searchBar
+            searchBar.sizeToFit()
+            searchBar.placeholder = "Search for places"
+            navigationItem.titleView = resultSearchController?.searchBar
+            resultSearchController?.hidesNavigationBarDuringPresentation = false
+            resultSearchController?.dimsBackgroundDuringPresentation = true
+            definesPresentationContext = true
+            
+            // Set LocationSearchTable current Map View
+            locationSearchTable.mapView = mapView
+            
+            // Handle incomming Map Searches
+            locationSearchTable.handleMapSearchDelegate = self
         }
         
     }
@@ -31,7 +56,7 @@ extension ViewController: CLLocationManagerDelegate {
 
     // Handle incoming location events.
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.first {
+        if let location = locations.last {
             // Set view center and region
             let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpanMake(0.05, 0.05))
@@ -62,4 +87,24 @@ extension ViewController: CLLocationManagerDelegate {
         print("Error: \(error)")
     }
 
+}
+
+extension ViewController: HandleMapSearch {
+    func dropPinZoomIn(placemark:MKPlacemark){
+        // cache the pin
+        selectedPin = placemark
+        // clear existing pins
+        mapView.removeAnnotations(mapView.annotations)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = placemark.coordinate
+        annotation.title = placemark.name
+        if let city = placemark.locality,
+            let state = placemark.administrativeArea {
+            annotation.subtitle = "\(city) \(state)"
+        }
+        mapView.addAnnotation(annotation)
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        let region = MKCoordinateRegionMake(placemark.coordinate, span)
+        mapView.setRegion(region, animated: true)
+    }
 }
